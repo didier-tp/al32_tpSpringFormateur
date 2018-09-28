d5f776f338ac8ff3a9f3127759ad92efd8c24596
didier-defrance <didier@d-defrance.fr>
2018-09-28 11:53:32 +0200

    debut jpa

--- src/main/java/fr/afcepf/al32/entity/Personne.java
+++ src/main/java/fr/afcepf/al32/entity/Personne.java
@@ -1,5 +1,8 @@
 package fr.afcepf.al32.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,8 +10,11 @@ import lombok.Setter;
 import lombok.ToString;
 
 @Getter @Setter @ToString @NoArgsConstructor @AllArgsConstructor
+
+@Entity
 public class Personne {
 
+	@Id
 	private Long numero; //id (pk)
 	private String prenom;
 	private String nom;
