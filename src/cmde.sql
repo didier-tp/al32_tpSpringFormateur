85031e259cd31c7412e780a6cd0914d08fa36a80
didier-defrance <didier@d-defrance.fr>
2018-09-28 15:42:55 +0200

    avec scripts mysql

---
+++ src/cmde.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE IF NOT EXISTS minibankdb;
+use minibankdb;
+show tables;
+describe personne;
+select * from personne;
---
+++ src/lancerCmdeMysql.bat
@@ -0,0 +1,6 @@
+cd /d "%~dp0"
+set MYSQL_HOME=C:\Program Files\MariaDB 10.3
+
+"%MYSQL_HOME%/bin/mysql" --port=3306 -u root -p <  cmde.sql
+
+pause
