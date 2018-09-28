f68e223dea1c23a3c1f78a1ffe9eef0e6b536d3e
didier-defrance <didier@d-defrance.fr>
2018-09-28 14:41:53 +0200

    avec config jpa xml

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO personne(numero,prenom,nom,email) VALUES(1,'olie','Condor','olie.condor@gmail.com');
+INSERT INTO personne(numero,prenom,nom,email) VALUES(2,'alain','Therieur','at@gmail.com');
